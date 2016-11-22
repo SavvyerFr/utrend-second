dbc9e9b5d6911c7a38a82361f2b50e0461056b5c
Dmytro <fokshei.d@gmail.com>
2016-11-22 07:04:46 +0000

    1

---
+++ js/share.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+function openWin1() {
+	myWin = open("https://plus.google.com/share?url=https%3A//savvyerfr.github.io/Utrend/index.html", "displayWindow", "width=520,height=300,left=350,top=170,status=no,toolbar=no,menubar=no");
+    };
+function openWin2() {
+    myWin = open("http://www.facebook.com/sharer.php?u=https://savvyerfr.github.io/Utrend/index.html", "displayWindow", "width=520,height=300,left=350,top=170,status=no,toolbar=no,menubar=no");
+};
+function openWin3() {
+    myWin = open("https://twitter.com/home?status=https%3A//savvyerfr.github.io/Utrend/index.html", "displayWindow", "width=520,height=300,left=350,top=170,status=no,toolbar=no,menubar=no");
+};
+function openWin4() {
+    myWin = open("https://pinterest.com/pin/create/button/?url=https%3A//savvyerfr.github.io/Utrend/index.html&media=&description=", "displayWindow", "width=520,height=300,left=350,top=170,status=no,toolbar=no,menubar=no");
+};
